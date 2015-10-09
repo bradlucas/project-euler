ce7350f9d7ca95e640957a0a0e44e5e49d142f8a
Brad Lucas <brad@beaconhill.com>
2015-10-09 18:19:55 -0400

    command line run problem

--- src/project_euler/core.clj
+++ src/project_euler/core.clj
@@ -1,7 +1,23 @@
 (ns project-euler.core
   (:gen-class :main true))
 
+(defn run-problem
+  [i]
+    (let [number (format "%03d" i)
+          function (symbol (str "problem-" number))]
+      (use (vec (list (symbol (str "project-euler.problem-" number)))))
+      (println (eval (list function)))))
+
+(def completed-problem-numbers (concat (range 1 26) '(41 67)))
 
 (defn -main
   [& args]
-  (println "Hello"))
+  (if (and args (= 1 (count args)))
+    (let [i (Integer/parseInt (first args))
+          valid (some #(= i %) completed-problem-numbers)]
+      (if valid
+        (run-problem i)
+        (println "Completed problem numbers are " (clojure.string/join " " completed-problem-numbers))
+        )
+      )
+    (println "Enter number: 1-25, 41 or 67")))
