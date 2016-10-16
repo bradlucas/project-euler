5871de33b3f94f611e07be29eec3b07c3a2bc76f
Brad Lucas <brad@beaconhill.com>
2016-10-15 22:22:08 -0400

    version 1.8.0 of clojure

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject project-euler "0.1.0-SNAPSHOT"
+(defproject project-euler "1.0.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :main project-euler.core)
